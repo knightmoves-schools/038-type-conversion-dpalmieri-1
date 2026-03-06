abaa6c52565506e3313336eb20a77ce089e6763c
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-03-06 11:42:29 -0600

    Implement implicit and explicit conversion methods

--- Converter.cs
+++ Converter.cs
@@ -2,7 +2,13 @@
 
 public class Converter{
    //Add your code here
-   public string Return(){
-      return "return";
+   public double ConvertImplicitly(int Number){
+      double Value = Number;
+      return Value;
+   }
+   public int ConvertExplicity(double Doubler){
+      int Converted = (int)Doubler;
+      return Converted;
    }
 }
+
