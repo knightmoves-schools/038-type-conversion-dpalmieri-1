4a046ff1cd3886d28548a92a12a53c43426b8289
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-03-06 11:44:06 -0600

    Refactor variable declarations in Converter methods
    Refactor variable declarations to be inline within methods.

--- Converter.cs
+++ Converter.cs
@@ -3,12 +3,13 @@
 public class Converter{
    //Add your code here
    public double ConvertImplicitly(int Number){
-      double Value = Number;
+      public double Value = Number;
       return Value;
    }
    public int ConvertExplicity(double Doubler){
-      int Converted = (int)Doubler;
+      public int Converted = (int)Doubler;
       return Converted;
    }
 }
+
 
