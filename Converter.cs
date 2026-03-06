7577881c2aace628efc998a5719a3d97ec2cea71
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-03-06 11:45:18 -0600

    Fix method definitions and variable declarations

--- Converter.cs
+++ Converter.cs
@@ -3,13 +3,14 @@
 public class Converter{
    //Add your code here
    public double ConvertImplicitly(int Number){
-      public double Value = Number;
+      double Value = Number;
       return Value;
    }
-   public int ConvertExplicity(double Doubler){
-      public int Converted = (int)Doubler;
+   public int ConvertExplicitly(double Doubler){
+      int Converted = (int)Doubler;
       return Converted;
    }
 }
+
 
 
